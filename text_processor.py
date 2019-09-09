a5130daf6e62c10db188bfa8cde7c1dca3b9d995
tezzytezzy <tezzy@hotmail.com>
2019-09-08 21:41:07 -0700

    text processed

--- text_processor.py
+++ text_processor.py
@@ -1,23 +1,40 @@
 ###
 #
 ###
-read_data("sample.in")
 
 def read_data(input_file):
     with open (input_file, 'rt', encoding="ISO-8859-1") as myfile:
+        start_idx_pos_list = []
         first_line = True
+        second_line = False
 
         for a_line in myfile:
-            if first_line :
-                text_string = a_line.split()
+            if first_line and (not second_line):
+                text_string = a_line
                 first_line = False
-            else:
+                second_line = True
+            elif (not first_line) and second_line:
                 question_num_and_length_of_text = a_line.split()
+                length_of_text = int(question_num_and_length_of_text[1])
+                second_line = False
+            else: # Neither first nor second line
+                start_idx_pos_list.append(int(a_line) - 1)
 
-        for num in range(question_num_and_length_of_text[0]):
-            
+    get_substrings(text_string, length_of_text, start_idx_pos_list)
+
+def get_substrings(text_string, length_of_text, start_idx_pos_list):
+    for q in range(len(start_idx_pos_list)):
+        string_to_examine = text_string[start_idx_pos_list[q]: start_idx_pos_list[q] + length_of_text].split()
+
+        letters = {}
+
+        # Exmaine all the combos of one-letter long to 'length_of_text'-letter long in 'string_to_examine'
+        for string_length in range(length_of_text):
 
 
 
 
-    count_pita_pizza_combo(month_profit, pita_profit, pizza_profit)
+        for letter in range(len(string_to_examine)):
+            pass
+    print ('x')
+read_data("sample.in")
